eb7a0e6f3c9072a246fc378a22458136318edef7
Mech654 <drahmet2007@hotmail.com>
2026-01-25 21:30:50 +0100

    game invite to other players

--- backend/lobby/lobby.go
+++ backend/lobby/lobby.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/http"
 	"sync"
+	"sync/atomic"
+	"time"
 
 	"github.com/Mech654/chess-server/backend/auth"
 	"github.com/gorilla/websocket"
@@ -14,15 +16,82 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true }, // Allow all origins for dev
 }
 
+var (
+	invite_counter uint64
+	match_invites  = make(map[uint64]*MatchInvite)
+	invites_mutex  sync.Mutex
+)
+
 type Player struct {
 	username string
 	conn     *websocket.Conn
 	send     chan []byte
+	handler  MessageHandler
 }
 
 type Lobby struct {
 	players map[*websocket.Conn]*Player
 	mutex   sync.Mutex
+}
+
+type MessageHandler interface {
+	HandleMessage(p *Player, data []byte)
+}
+
+type LobbyHandler struct {
+	parentLobby *Lobby
+}
+
+type Match struct {
+	match_invite MatchInvite
+}
+
+type MatchInvite struct {
+	ID          uint64
+	from_player *Player
+	to_player   *Player
+	created_at  time.Time
+}
+
+// DTOs
+
+type MatchInviteDTO struct {
+	From string `json:"from"`
+	To   string `json:"to"`
+}
+
+func HelperMarshal(data interface{}) []byte {
+	b, err := json.Marshal(data)
+	if err != nil {
+		log.Printf("CRITICAL: Marshal failed: %v", err)
+		return []byte("{}")
+	}
+	return b
+}
+
+func HelperEnvelopeMarshal(msgType string, data interface{}) []byte {
+	envelope := map[string]interface{}{
+		"type": msgType,
+		"data": data,
+	}
+	return HelperMarshal(envelope)
+}
+
+func HelperUnmarshal(data []byte, v any) error {
+	err := json.Unmarshal(data, v)
+	if err != nil {
+		log.Printf("CRITICAL: Unmarshal failed: %v", err)
+		return err
+	}
+	return nil
+}
+
+func (mi *MatchInvite) Timer() {
+	time.Sleep(30 * time.Second)
+	invites_mutex.Lock()
+	defer invites_mutex.Unlock()
+
+	delete(match_invites, mi.ID)
 }
 
 func (c *Player) writePump() {
@@ -87,9 +156,18 @@ func (l *Lobby) ServeWS(w http.ResponseWriter, r *http.Request) {
 
 	for {
 		// Keep the connection alive
-		_, _, err := conn.ReadMessage()
+		_, message, err := conn.ReadMessage()
 		if err != nil {
 			break
+		}
+
+		l.mutex.Lock()
+		p := l.players[conn]
+		handler := p.handler
+		l.mutex.Unlock()
+
+		if handler != nil {
+			handler.HandleMessage(p, message)
 		}
 	}
 }
@@ -116,19 +194,80 @@ func (l *Lobby) broadcastPlayerList() {
 		case player.send <- message:
 			// Message queued successfully
 		default:
-			// Mailbox is full
-			close(player.send)
+			// Mailbox is full, kill player. Dont do this irl.
+			player.conn.Close()
 		}
 	}
 }
 
-// for {
-// 	// Read messages
-// 	_, message, err := conn.ReadMessage()
-// 	if err != nil {
-// 		break
-// 	}
-//
-// 	var envelope WSmessage
-// 	json.Unmarshal(message, &envelope)
-// }
+func (lh *LobbyHandler) HandleMessage(p *Player, data []byte) {
+	var envelope Envelope
+	err := json.Unmarshal(data, &envelope)
+	if err != nil {
+		log.Println("Error unmarshaling message:", err)
+		return
+	}
+
+	switch envelope.Type {
+	case "MATCH_INVITE":
+		err, toPlayer, inviteDTO := lh.NewMatchInvite(p, envelope.Data)
+		if err != nil {
+			log.Println("Error creating MatchInvite:", err)
+			return
+		}
+
+		toPlayer.send <- HelperEnvelopeMarshal("MATCH_INVITE", inviteDTO)
+	case "MATCH_ACCEPT":
+	}
+
+}
+
+func (lh *LobbyHandler) NewMatchInvite(p *Player, data json.RawMessage) (error, *Player, *MatchInviteDTO) {
+	newID := atomic.AddUint64(&invite_counter, 1)
+
+	var inviteDTO MatchInviteDTO
+	HelperUnmarshal(data, &inviteDTO)
+	inviteDTO.From = p.username
+
+	toPlayer := findPlayerByUsername(lh.parentLobby, inviteDTO.To)
+	if toPlayer == nil {
+		return nil, nil, nil
+	}
+
+	invite := MatchInvite{
+		ID:          newID,
+		from_player: p,
+		to_player:   toPlayer,
+		created_at:  time.Now(),
+	}
+
+	invites_mutex.Lock()
+	match_invites[newID] = &invite
+	invites_mutex.Unlock()
+
+	go invite.Timer()
+
+	return nil, toPlayer, &inviteDTO
+}
+
+func (lh *LobbyHandler) NewMatch(p1_username string, p2_username string) *Match {
+	return nil
+
+}
+
+func findPlayerByUsername(l *Lobby, username string) *Player {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+
+	for _, player := range l.players {
+		if player.username == username {
+			return player
+		}
+	}
+	return nil
+}
+
+type Envelope struct {
+	Type string          `json:"type"`
+	Data json.RawMessage `json:"data"`
+}
