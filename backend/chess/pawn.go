c45ec5d9306c30e8b11c4ae19550480e8a052945
Mech654 <drahmet2007@hotmail.com>
2026-02-17 21:01:02 +0100

    Finished Pawn

--- backend/chess/pawn.go
+++ backend/chess/pawn.go
@@ -21,13 +21,18 @@ func (p *Pawn) GetMobility() Mobility {
 }
 
 // For pawn we need a slightly different approach as its capture method conflicts with its normal movement, so we need one to be true not all.
-func (p *Pawn) IsLegalPieceMove(move Move, board *Board) bool {
-	valid := checkBasicMobility(p.GetMobility(), move, board)
+func (p *Pawn) IsLegalPieceMove(move Move, gameState *GameState) bool {
+	valid := checkBasicMobility(p.GetMobility(), move, &gameState.Board)
 	if valid {
 		return true
 	}
 
-	valid = checkCapture(move, board)
+	valid = checkBasicCapture(move, &gameState.Board)
+	if valid {
+		return true
+	}
+
+	valid = checkEnpassant(move, gameState)
 	return valid
 }
 
@@ -43,15 +48,31 @@ func checkBasicMobility(mobility Mobility, move Move, board *Board) bool {
 	if dx != mobility.X {
 		return false
 	}
-	if dy != mobility.Y {
-		return false
+
+	if move.Player == 1 {
+		if dy != mobility.Y && dy != 2 {
+			return false
+		}
+
+		if dy == 2 && move.PosFrom.Y != 1 {
+			return false
+		}
+	} else if move.Player == 2 {
+		if dy != -mobility.Y && dy != -2 {
+			return false
+		}
+
+		if dy == -2 && move.PosFrom.Y != 6 {
+			return false
+		}
 	}
+
 	return true
 }
 
-func checkCapture(move Move, board *Board) bool {
-	destPiece := board[move.PosTo.X][move.PosTo.Y]
-	if destPiece.Owner == 3 {
+func checkBasicCapture(move Move, board *Board) bool {
+	destSquare := board.GetSquareAt(move.PosTo)
+	if destSquare.Owner == 3 {
 		return false
 	}
 
@@ -61,8 +82,82 @@ func checkCapture(move Move, board *Board) bool {
 	if int(math.Abs(float64(dx))) != 1 {
 		return false
 	}
-	if dy != 1 {
-		return false
+
+	if move.Player == 1 {
+		if dy != 1 {
+			return false
+		}
+	} else if move.Player == 2 {
+		if dy != -1 {
+			return false
+		}
 	}
+
 	return true
 }
+
+func checkEnpassant(move Move, gameState *GameState) bool {
+	destSquare := gameState.Board.GetSquareAt(move.PosTo)
+	if destSquare.Owner != 3 {
+		return false
+	}
+
+	dx := move.PosTo.X - move.PosFrom.X
+	dy := move.PosTo.Y - move.PosFrom.Y
+
+	if dx != 1 && dx != -1 {
+		return false
+	}
+
+	if (dy != 1 && move.Player != 1) {
+		return false
+	}
+
+	if (dy != -1 && move.Player != 2){
+		return false
+	}
+
+	adjacentSquare := gameState.Board.GetSquareAt(Coordinates{X: move.PosTo.X, Y: move.PosFrom.Y})
+	if adjacentSquare.Type != PawnType {
+		return false
+	}
+
+	if adjacentSquare.Owner == move.Player {
+		return false
+	}
+
+	latestMove := gameState.MoveRecords.GetLastMove()
+	if latestMove == nil {
+		return false
+	}
+
+	if latestMove.Player != adjacentSquare.Owner {
+		return false
+	}
+
+	if latestMove.PosFrom.X != move.PosTo.X {
+		return false
+	}
+
+	if move.Player == 1 {
+		if latestMove.PosFrom.Y != 6 || latestMove.PosTo.Y != 4 {
+			return false
+		}
+
+		if move.PosTo.Y != 5 {
+			return false
+		}
+
+	} else if move.Player == 2 {
+		if latestMove.PosFrom.Y != 1 || latestMove.PosTo.Y != 3 {
+			return false
+		}
+
+		if move.PosTo.Y != 2 {
+			return false
+		}
+	}
+
+	return true
+}
+
