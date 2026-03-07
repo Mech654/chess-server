799c83dcfe290dae8d430fd96632f0a2e3c9949a
Mech654 <drahmet2007@hotmail.com>
2026-03-08 00:04:14 +0100

    No Cache - It Made Issues Fr  Buzzin

--- frontend-stuff/static-server.go
+++ frontend-stuff/static-server.go
@@ -4,7 +4,16 @@ import (
 	"net/http"
 )
 
+func noCache(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+		w.Header().Set("Pragma", "no-cache")
+		w.Header().Set("Expires", "0")
+		next.ServeHTTP(w, r)
+	})
+}
+
 func RegisterRoutes(mux *http.ServeMux) {
 	fs := http.FileServer(http.Dir("frontend-stuff/static/"))
-	mux.Handle("/", fs)
+	mux.Handle("/", noCache(fs))
 }
